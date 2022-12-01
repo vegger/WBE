13b3c47b971d17c7c890611a077feace0afb0dbb
gabricyr <gabricyr@students.zhaw.ch>
2022-12-01 16:04:12 +0100

    WIP Cyril

--- Praktikum10/connect4-winner.js
+++ Praktikum10/connect4-winner.js
@@ -2,7 +2,7 @@
 
 function connect4Winner(color, board) {
   //check rows
-  for(let rowNr = 0; rowNr < board.length; rowNr++) {
+  /*for(let rowNr = 0; rowNr < board.length; rowNr++) {
     let count = 0
     let last = ''
     for(let colNr = 0; colNr < board[rowNr].length; colNr++) {
@@ -26,18 +26,57 @@ function connect4Winner(color, board) {
       //console.log("ROW: ", rowNr, " COL: ", colNr, "COLOR: ", board[rowNr][colNr], "COUNT: ", count)
     }
     count = 0
+  }*/
+
+  //check diagonal descending 
+  for(let rowNr = 0; rowNr < board[1].length; rowNr++) {
+    let count = 0;
+    let last = '';
+
+    for(let colNr = 0; colNr < board[0].length; colNr++) {
+      if(board[rowNr][colNr] == color) {
+        count++;
+        for(let nextRowNr = rowNr + 1; nextRowNr < board.length; nextRowNr++) {
+          for(let nextColNr = colNr + 1; nextColNr < board[0].length; nextColNr++){
+            if(board[nextRowNr][nextColNr] == color) count++
+            else count = 0
+            last = board[rowNr][colNr]
+            if(count >= 4) return true
+            break
+          }
+        }
+      }
+    }
+    count = 0
   }
 
-  //check diagonal
-  //TODO
+  for(let colNr = 0; colNr < board[0].length; ++colNr) {
+    let count = 0; 
+    let last = '';
+    for(let rowNr = 0; rowNr < board[1].length; rowNr++) {
+      if(board[rowNr][colNr] == color) {
+        count++;
+        for(let nextColNr = colNr + 1; nextColNr < board[0].length; ++nextColNr) {
+          for(let nextRowNr = rowNr + 1; nextRowNr < board[1].length; ++nextRowNr){
+            if(board[nextColNr][nextRowNr] == color) count++
+            else count = 0
+          }
+        }
+      }
+    }
+    
+  }
 
 
   return false
 }
 
-console.log(connect4Winner('r', [[ '_', '_', '_', '_', '_', '_', '_' ],
+console.log(connect4Winner('r', [
+[ '_', '_', '_', '_', '_', '_', '_' ],
 [ '_', '_', '_', '_', 'r', '_', '_' ],
 [ '_', '_', '_', '_', '_', '_', '_' ],
 [ '_', '_', '_', 'r', 'r', 'b', 'b' ],
 [ '_', 'r', 'r', 'b', 'r', 'r', 'b' ],
 [ 'b', 'b', 'b', 'r', 'r', 'r', 'b' ]]))
+
+module.exports = { connect4Winner }
