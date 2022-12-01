77a1eace319dc4d80402b809e8b61d3f022c9768
Valentin Egger <eggervali@gmail.com>
2022-12-01 11:21:58 +0100

    check columns

--- Praktikum10/connect4-winner.js
+++ Praktikum10/connect4-winner.js
@@ -2,24 +2,42 @@
 
 function connect4Winner(color, board) {
   //check rows
-  for(let i = 0; i < board.length; i++) {
+  for(let rowNr = 0; rowNr < board.length; rowNr++) {
     let count = 0
     let last = ''
-    for(let j = 0; j < board[i].length; j++) {
-      if((last == color || j == 0) && board[i][j] == color) count++
+    for(let colNr = 0; colNr < board[rowNr].length; colNr++) {
+      if(board[rowNr][colNr] == color) count++
       else count = 0
-      last = board[i][j]
+      last = board[rowNr][colNr]
       if(count >= 4) return true
     }
     count = 0
   }
-  return false
+
   //check columns
+  for(let colNr = 0; colNr < board[0].length; colNr++) {
+    let count = 0
+    let last = ''
+    for(let rowNr = 0; rowNr < board.length; rowNr++) {
+      if(board[rowNr][colNr] == color) count++
+      else count = 0
+      last = board[rowNr][colNr]
+      if(count >= 4) return true
+      //console.log("ROW: ", rowNr, " COL: ", colNr, "COLOR: ", board[rowNr][colNr], "COUNT: ", count)
+    }
+    count = 0
+  }
+
+  //check diagonal
+  //TODO
+
+
+  return false
 }
 
-console.log(connect4Winner('b', [[ '_', '_', '_', '_', '_', '_', '_' ],
-[ '_', '_', '_', '_', '_', '_', '_' ],
+console.log(connect4Winner('r', [[ '_', '_', '_', '_', '_', '_', '_' ],
 [ '_', '_', '_', '_', 'r', '_', '_' ],
+[ '_', '_', '_', '_', '_', '_', '_' ],
 [ '_', '_', '_', 'r', 'r', 'b', 'b' ],
-[ '_', '_', 'r', 'b', 'r', 'r', 'b' ],
+[ '_', 'r', 'r', 'b', 'r', 'r', 'b' ],
 [ 'b', 'b', 'b', 'r', 'r', 'r', 'b' ]]))
