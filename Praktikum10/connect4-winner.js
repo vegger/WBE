6c68adcc75717fe35b663d51152af2552d25ea93
Valentin Egger <eggervali@gmail.com>
2022-12-01 08:49:35 +0100

    bugfix

--- Praktikum10/connect4-winner.js
+++ Praktikum10/connect4-winner.js
@@ -2,18 +2,18 @@
 
 function connect4Winner(color, board) {
   //check rows
-  console.log(board[4][4])
   for(let i = 0; i < board.length; i++) {
     let count = 0
     let last = ''
-    for(let j = 0; j < board[i].length; i++) {
-      if(last == color && board[i][j] == color) count++
+    for(let j = 0; j < board[i].length; j++) {
+      if((last == color || j == 0) && board[i][j] == color) count++
+      else count = 0
       last = board[i][j]
+      if(count >= 4) return true
     }
-    if(count >= 4) return true
     count = 0
   }
-
+  return false
   //check columns
 }
 
@@ -22,4 +22,4 @@ console.log(connect4Winner('b', [[ '_', '_', '_', '_', '_', '_', '_' ],
 [ '_', '_', '_', '_', 'r', '_', '_' ],
 [ '_', '_', '_', 'r', 'r', 'b', 'b' ],
 [ '_', '_', 'r', 'b', 'r', 'r', 'b' ],
-[ 'b', 'b', 'b', 'r', 'r', 'b', 'b' ]]))
+[ 'b', 'b', 'b', 'r', 'r', 'r', 'b' ]]))
