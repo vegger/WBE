4f479d13dea3c64d531f7bd8c79f99bc3a9d49bd
Valentin Egger <eggervali@gmail.com>
2022-12-15 15:04:44 +0100

    refactoring sjdon

--- Praktikum11/public/render-sjdon.js
+++ Praktikum11/public/render-sjdon.js
@@ -1,57 +1,21 @@
-function renderSJDON(element, appRoot) {
-    //TODO funktionskomponente (Vorlesung 11: S.40) NICHT implementiert
-    let node = document.createElement(element[0]);
-    appRoot.appendChild(node);
-    let currentElement = node;
-    for (i in element) {
-      if (typeof element[i] === "object" && !Array.isArray(element[i])) { //good 
-        //wenn object -> attribut z.B: style
-        let object = element[i];
-        let attributes = Object.keys(object);
-        let values = Object.values(object);
-        for (attribute of attributes) {
-          for (value of values) {
-            //console.log("CR", currentElement, attribute, value);
-            currentElement.setAttribute(attribute, value);
-            break; //nötig, weil sonst wenn mehrere attribute im object vorheriges immer überschrieben wird
+const renderSJDON = (element, appRoot) => {
+  const [type, ...sjdon] = element
+  const node = document.createElement(type)
+
+  for (let item of sjdon) {
+      if (item && typeof(item) == 'object' && !Array.isArray(item)) {
+          for (let attributeName of Object.keys(item)) {
+              node[attributeName] = item[attributeName]
           }
-        }
+      } else if (Array.isArray(item)) {
+          renderSJDON(item, node)
+      } else {
+          node.appendChild(document.createTextNode(item))
       }
-      if (Array.isArray(element[i])) {
-        //wenn array -> neues HTML element
-        array = element[i];
-        topItem = document.createElement(array[0]);
-        currentElement.appendChild(topItem);
-        for (arrayElement of array) {
-          if(typeof arrayElement === "string") {
-            topItem.textContent = arrayElement
-          }
-          if(typeof arrayElement === "object" && !Array.isArray(arrayElement)) {
-            //console.log(arrayElement)
-            let object = arrayElement;
-            let attributes = Object.keys(object);
-            let values = Object.values(object);
-            for (attribute of attributes) {
-              for (value of values) {
-                //console.log("CR", currentElement, attribute, value);
-                topItem.setAttribute(attribute, value);
-                break; //nötig, weil sonst wenn mehrere attribute im object vorheriges immer überschrieben wird
-              }
-            }
-          }
-          // weiss nicht ob man das braucht Zeile 221 ff. für verschachtelte Elemente... jedoch sowiso refactoring nötig hier dann einfach nochmals element-erzeugungsmethode aufrufen
-          /*if (Array.isArray(arrayElement[i])) {
-            console.log("in Zeile 205")
-            //HTML-Element in HTML-Element (nested elements)
-            item = document.createElement(arrayElement[i][0]);
-            item.textContent = arrayElement[i][1];
-            console.log("TEXT: ");
-            topItem.appendChild(item);
-          }*/
-        }
-      }
-    }
   }
+
+  appRoot.appendChild(node)
+}
 
 const element =
 ["div", {style: "background: salmon"},
@@ -62,4 +26,3 @@ function initSJDON() {
     const appRoot = document.getElementsByClassName("sjdon")[0];
     renderSJDON(element, appRoot);
 }
-
