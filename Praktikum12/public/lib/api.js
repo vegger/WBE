c1b8d8da7662332ff6a1a2aa86c95a8a7a5415f3
Valentin Egger <eggervali@gmail.com>
2022-12-23 15:28:34 +0100

    fix api

--- Praktikum12/public/lib/api.js
+++ Praktikum12/public/lib/api.js
@@ -2,7 +2,7 @@ const SERVICE = "http://localhost:3000/api/data/1234567890?api-key=c4game"
 
 export async function loadState() {
   try {
-    return await fetch("http://localhost:3000/api/data/1234567890?api-key=c4game").then((r) => r.json())
+    return await fetch(SERVICE).then((r) => r.json())
   } catch {
     alert("Load not possible, can't connect to server")
   }
@@ -12,9 +12,9 @@ export function loadStateFromLocalStorage() {
   return JSON.parse(localStorage.getItem("state"))
 }
 
-export function saveState(state) {
+export async function saveState(state) {
   try {
-    fetch("http://localhost:3000/api/data/1234567890?api-key=c4game", {
+    await fetch(SERVICE, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(state),
